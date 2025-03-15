5efee908926c5d75ba31d19d0697f6d2dee43962
Nolan Nguyen! <nolan2087@gmail.com>
2025-03-14 18:16:20 -0700

    changing data model to include array of image addresses and created upload pipeline to supabase storage for meet images, as well as interface for doing so on create page

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -5,25 +5,45 @@ import { Input, Textarea, Alert} from "@heroui/react";
 import { useRouter } from 'next/navigation'
 import Image from "next/image";
 
-import { useState } from "react";
+import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import { initMap, addEventMarkers } from "../../api/mapbox";
 
+import { useState, useRef, useEffect } from "react";
 import { supabase } from '@/clients/supabaseClient'
+import Meet from '@/models/meet'
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoibm5kcHpubiIsImEiOiJjbTZxNmF1NjgxbDV5MmxwemlxOG13OG1lIn0.Akl1Y0JXLXH6eB0R6z9wkQ'
+
 
 export default function Create() {
 	const router = useRouter()
+	const mapContainer = useRef<HTMLDivElement | null>(null);
+	const map = useRef<mapboxgl.Map | null>(null);
 
 	const [title, setTitle] = useState('')
-	const [location, setLocation] = useState([-87.616, 41.776])
+	const [address, setAddress] = useState('')
+	const [location, setLocation] = useState([-87.616, 41.776]) 
 	const [body, setBody] = useState('')
 	const [links, setLinks] = useState('')
+	const [imageFiles, setImageFiles] = useState<File[]>([])
 
 	const [incAlertVisible, setIncAlertVisible] = useState(false)
 
+	const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const files = e.target.files;
+		if (files) {
+		  setImageFiles(Array.from(files)); // Convert FileList to an array
+		}
+	  };
+
 	const handleClear = () => {
 		setTitle('')
-		setLocation([-87.616, 41.776])
+		setAddress('')
 		setBody('')
 		setLinks('')
+		setImageFiles([])
+		setLocation([-87.616, 41.776])
 
 	}
 	const handleSubmit = async (e:any) => {
@@ -35,40 +55,14 @@ export default function Create() {
 			return
 		}
 
-		// const {
-		// 	data: { session },
-		// 	error: sessionError,
-		// } = await supabase.auth.getSession()
+		const meet = new Meet(title, body, links, [-87.616, 41.776])
 
-		// if (sessionError || !session) {
-		// 	console.log('session not found')
-		// 	return
-		//   }
+		await meet.uploadImages(imageFiles)
 
-		// const userID = session.user.id
+		await meet.saveToDatabase()
 
-		const { data, error } = await supabase
-			.from('meets')
-			.insert([
-				{
-					organizer_id: 0,
-					title: title,
-					body: body,
-					location: [-87.616, 41.776],
-					links: links
-				}
-			])
-			.select()
+		console.log('form data submitted successfully.', meet)
 
-		if (error) {
-			console.log("uh oh. submission error.", error)
-			return
-		}
-
-		if (data) {
-			console.log(data)
-			console.log('form data submitted successfully.', title, body, links)
-		}
 	}
 
 	return (
@@ -81,31 +75,44 @@ export default function Create() {
 				{/* <Button color="primary" className="absolute top-10 right-4 m-4 hover:text-red-400" type="button" onPress={() => router.push("/create")}>new meet</Button> */}
 			</div>
 
-			<div className="mx-[10vw] mt-20 h-full">
-
+			<div className="mx-[10vw] mt-5 h-full">
 				<h1 id="header" className="text-3xl font-bold mb-5">New Meet</h1>
 
-				<div className="flex">
+				<div className="flex mb-5">
 					<div id="fields" className="w-2/5">
-						<p className="mt-5 text-xl font-bold">Title</p>
+						<p className="text-xl font-bold">Title</p>
 						<Input value={title} onChange={e => setTitle(e.target.value)}size="md" type="text" />
+						
 						<p className="mt-5 text-xl font-bold">Body</p>
-						<Textarea value={body} onChange={e => setBody(e.target.value)} size="md" type="text" />
+						<Textarea minRows={4} maxRows={4} value={body} onChange={e => setBody(e.target.value)} size="md" type="text" />
+						
 						<p className="mt-5 text-xl font-bold">Location</p>
-						<Input size="md" type="text" value={location.join(", ")} onChange={(e) => setLocation(e.target.value.split(",").map(Number))}/>
+						<Input value={address} onChange={e => setAddress(e.target.value)}size="md" type="text" />
+						
 						<p className="mt-5 text-xl font-bold">Links (Optional)</p>
 						<Input value={links} onChange={e => setLinks(e.target.value)}size="md" type="text" />
 					</div>
 
-					<div id="images" className="w-3/5 m-10 h-full">
-						<div className="p-40 bg-[#171717] border border-red-400 rounded">
-							wsg bb	
+					<div id="images" className="w-3/5 ml-10">
+						<p className="text-xl font-bold">Upload Images</p>
+				
+						<div className="mt-4 p-40 bg-[#171717] border border-red-400 rounded">
+
+							<input
+							className=""
+							type="file"
+							multiple
+							accept="image/*"
+							onChange={handleImageChange}
+							/>
+
+							<p>yes we know this looks not great</p>
 						</div>
 					</div>
 				</div>
 
 				<Button color="primary" className="mx-3 bg-primary-700" onPress={handleClear}>Clear</Button>
-				<Button color="primary" className="mx-3">Save Draft</Button>
+				<Button color="primary" isDisabled={true} className="mx-3">Save Draft</Button>
 				<Button color="primary" className="mx-3" onPress={handleSubmit}>Submit</Button>
 
 				<Alert className="mt-5" title="Incomplete Meet"  description="Your meet is incomplete. Please fill out the required sections." isVisible={incAlertVisible}  onClose={() => setIncAlertVisible(false)}/>
