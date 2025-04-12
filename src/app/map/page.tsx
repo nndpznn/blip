60dc712a06481e2f50ebe9a7fe6bded98ecc4f40
Nolan Nguyen! <nolan2087@gmail.com>
2025-04-11 23:16:29 -0700

    creating searchbar component, hooking it up to map(which is now displayed indefinitely on our beloved map page)

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -4,7 +4,8 @@
 import { useRouter } from 'next/navigation';
 import React, { useEffect, useRef } from "react";
 import { initMap, addEventMarkers } from "../../api/mapbox";
-
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import mapboxgl from 'mapbox-gl';
 // components
 import {Button, ButtonGroup} from "@heroui/button";
 import Link from 'next/link';
@@ -12,6 +13,7 @@ import Image from "next/image";
 
 // customs
 import { Event } from "../../types/event"
+import Searchbar from '@/components/searchbar';
 
 interface MapProps {
 	events: Event[]
@@ -21,29 +23,48 @@ export default function Map({ events = [] }: MapProps) {
 	const router = useRouter()
 
 	const mapContainerRef = useRef<HTMLDivElement>(null);
+	const mapRef = useRef<mapboxgl.Map | null>(null);
 
- 	// useEffect(() => {
-	// 	if (!mapContainerRef.current) return;
 
-    // 	const map = initMap(mapContainerRef.current.id);
+ 	useEffect(() => {
+		if (!mapContainerRef.current) return;
+
+    	const map = initMap(mapContainerRef.current.id);
+		mapRef.current = map;
+
+
+		// const geocoder = new MapboxGeocoder({
+		// 	accessToken: mapboxgl.accessToken!,
+		// 	mapboxgl,
+		// 	placeholder: "Search for an address",
+		// 	marker: false,
+		// 	flyTo: { zoom: 14 },
+		// });
 		
-	// 	// Once the map is loaded, add markers
-	// 	if (events.length > 0) {
-	// 		map.on("load", () => {
-	// 			addEventMarkers(map, events);
-	// 		});
-	// 	}
+		// Once the map is loaded, add markers
+		if (events.length > 0) {
+			map.on("load", () => {
+				addEventMarkers(map, events);
+			});
+		}
 
 
-	// 	return () => map.remove(); // Cleanup
-  	// }, [events]);
+		return () => map.remove(); // Cleanup
+  	}, [events]);
 
 	return (
 		<div className="flex flex-col w-full h-full">
 
-			<div className="flex-1 w-full"> {/* could be border-t-4 */}
-				{/* <div id="map" ref={mapContainerRef} className="flex w-[100vw] h-[70vh] text-center overflow-hidden" />   */}
-				<div id="mapreplacement" className="flex w-[100vw] h-[70vh] text-center bg-gray-500 overflow-hidden"/>
+			<div className="flex-1 w-full">
+				<div className="absolute z-10">
+					<Searchbar onSelect={(place) => {
+						const [lng, lat] = place.center;
+						mapRef.current?.flyTo({ center: [lng, lat], zoom: 14 });
+					}} />
+				</div>
+
+				<div id="map" ref={mapContainerRef} className="flex w-[100vw] h-[70vh] text-center overflow-hidden" />  
+				{/* <div id="mapreplacement" className="flex w-[100vw] h-[70vh] text-center bg-gray-500 overflow-hidden"/> */}
 			</div>
 
 			<div className="flex justify-center justify-between">
